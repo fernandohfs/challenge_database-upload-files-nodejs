a02c6e4863cf1bc634d9129b7bb1f26f45f91c71
Fernando Soares <fernando.hfs94@gmail.com>
2020-04-15 16:46:20 -0300

    :sparkles: Add POST /transactions endpoint

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,35 @@
 import { Router } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  return response.json({ ok: true });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  const transaction = await CreateTransactionService.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.status(201).json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  return response.json({ ok: true });
 });
 
 transactionsRouter.post('/import', async (request, response) => {
-  // TODO
+  return response.json({ ok: true });
 });
 
 export default transactionsRouter;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,63 @@
+import { getRepository } from 'typeorm';
+
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public static async execute({
+    title,
+    value,
+    type,
+    category,
+  }: RequestDTO): Promise<Transaction> {
+    const categoriesRepository = getRepository(Category);
+    const transactionsRepository = getRepository(Transaction);
+
+    /**
+     * 1ª Regra de negócio -> Verificar se a categoria já está cadastrada no banco de dados
+     */
+
+    const findCategory = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    let categoryId;
+
+    if (findCategory) {
+      categoryId = findCategory.id;
+    } else {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(newCategory);
+
+      categoryId = newCategory.id;
+    }
+
+    /**
+     * 2ª Regra de negócio -> cadastrar uma nova transação com o categoryId tratado acima
+     */
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: categoryId,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
